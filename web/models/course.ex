86c84448423e898e5bbc8c75ae70ca2725bf7bfa
Jean Joeris <joeris.peter@gmail.com>
2017-01-12 16:54:46 -0700

    Add course, section and enrollment models with appropriate connections between them

--- web/models/course.ex
+++ web/models/course.ex
@@ -3,8 +3,34 @@ defmodule DbExperiment.Course do
 
   schema "courses" do
     field :name, :string
-    # field :created_at, :datetime
-    # field :updated_at, :datetime
+    field :created_at, :naive_datetime
+    field :updated_at, :naive_datetime
     field :program_id, :integer
+    has_many :sections, DbExperiment.Section
+    has_many :enrollments, through: [:sections, :enrollments]
+  end
+
+  def fetch_backend_sections do
+    query = "SELECT sections.* FROM sections INNER JOIN courses ON sections.course_id = courses.id WHERE courses.name = 'Object-Oriented Programming' AND (start_date > '2017-01-12')  ORDER BY sections.start_date ASC limit 3"
+    result = Ecto.Adapters.SQL.query(DbExperiment.Repo, query, [])
+    cols = Enum.map elem(result,1).columns, &(String.to_atom(&1))
+    roles = Enum.map elem(result,1).rows, fn(row) ->
+      struct(DbExperiment.Section, Enum.zip(cols, row))
+    end
+  end
+
+  def fetch_frontend_sections do
+    query = "SELECT sections.* FROM sections INNER JOIN courses ON sections.course_id = courses.id WHERE courses.name = 'Fundamental Web Technologies' AND (start_date > '2017-01-12')  ORDER BY sections.start_date ASC limit 3"
+    result = Ecto.Adapters.SQL.query(DbExperiment.Repo, query, [])
+    cols = Enum.map elem(result,1).columns, &(String.to_atom(&1))
+    roles = Enum.map elem(result,1).rows, fn(row) ->
+      struct(DbExperiment.Section, Enum.zip(cols, row))
+    end
+  end
+
+  def fetch_next_sections do
+    frontend_sections = fetch_frontend_sections
+    backend_sections = fetch_backend_sections
+    frontend_sections ++ backend_sections
   end
 end
--- web/models/section.ex
+++ web/models/section.ex
@@ -10,13 +10,31 @@
 #   end
 defmodule DbExperiment.Section do
   use DbExperiment.Web, :model
+  alias DbExperiment.Repo
 
   schema "sections" do
+    has_many :enrollments, DbExperiment.Enrollment
+    belongs_to :course, DbExperiment.Course
     field :name, :string
-    field :course_id, :integer
     field :status, :string
     field :max_number_of_students, :integer
     field :start_date, :date
     field :end_date, :date
+  end
+
+  def program_id(section_id) do
+    section = Repo.get(DbExperiment.Section, section_id)
+    course = Repo.preload(section, :course).course
+    course.program_id
+  end
+
+  def total_enrolled(section_id) do
+    query = "SELECT enrollments.* FROM enrollments WHERE enrollments.section_id = #{section_id}"
+    result = Ecto.Adapters.SQL.query(Repo, query, [])
+    cols = Enum.map elem(result,1).columns, &(String.to_atom(&1))
+    roles = Enum.map elem(result,1).rows, fn(row) ->
+      struct(DbExperiment.Section, Enum.zip(cols, row))
+    end
+    length roles
   end
 end
